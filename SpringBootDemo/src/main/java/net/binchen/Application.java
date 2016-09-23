fb66c052b89ddfbaff366fa69513a0cde230e2f3
BinChen <bingguichen@gmail.com>
2016-09-23 09:53:02 +0800

    update jobs

--- SpringBootDemo/src/main/java/net/binchen/Application.java
+++ SpringBootDemo/src/main/java/net/binchen/Application.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
@@ -17,6 +16,6 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		 SpringApplication.run(Application.class, args);
 	}
 }
